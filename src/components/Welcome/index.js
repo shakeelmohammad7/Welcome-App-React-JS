c81750689df2634cfc44dacd2a62be15c927bd34
shakeel <shakeel16@example.com>
2023-06-02 04:13:36 +0000

    CP-5

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,37 @@
+import {Component} from 'react'
+import './index.css'
+
+class Welcome extends Component {
+  state = {
+    isSubscribed: false,
+  }
+
+  onClickSubscribedButton = () => {
+    this.setState(prevState => ({isSubscribed: !prevState.isSubscribed}))
+  }
+
+  getButtonText = () => {
+    const {isSubscribed} = this.state
+    return isSubscribed ? 'Subscribed' : 'Subscribe'
+  }
+
+  render() {
+    const buttonText = this.getButtonText()
+
+    return (
+      <div className="bg-container">
+        <h1 className="heading">Welcome</h1>
+        <p className="description">Thank you! Happy Learning</p>
+        <button
+          type="button"
+          className="btn"
+          onClick={this.onClickSubscribedButton}
+        >
+          {buttonText}
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Welcome
